01b064392792c6615bf4a91b921907d6610b4660
timetree <timetree@kakao.com>
2020-10-11 17:25:09 +0900

    jwt 토큰 라이브러리 추가, 현재 비활성화 상태 ID, 학원코드 입력으로 토큰 발급 처리 /solve 라우터에서 문제 노출 순서 세팅

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -13,12 +13,22 @@ router.post('/register/:Gr', async (req, res, next) => {
     res.status(400).json({ 'status': 'error', 'msg': err.message });
   }
 });
-
+/*  현재 미니 프로젝트 단계에서는 아이디 비밀번호 로그인 X, 향후 활용 가능성 있어 남겨둠
 router.post('/check', async (req, res, next) => {
   const { userId, userPw } = req.body;  // 유저 ID, 유저 PW
   try {
     if (is.empty(userId) || is.empty(userPw)) throw new Error('필수 파라미터가 없습니다.');
     res.status(200).json(await authMd.check(userId, userPw));
+  } catch (err) {
+    res.status(400).json({ 'status': 'error', 'msg': err.message });
+  }
+});
+*/
+router.post('/check', async (req, res, next) => {
+  const { userId, academyCode } = req.body;  // 유저 ID, 유저 PW
+  try {
+    if (is.empty(userId) || is.empty(academyCode)) throw new Error('필수 파라미터가 없습니다.');
+    res.status(200).json(await authMd.check(userId, academyCode));
   } catch (err) {
     res.status(400).json({ 'status': 'error', 'msg': err.message });
   }
